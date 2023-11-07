5593c103b9282d393b787c34a82fc8361d9f30b5
Massoud Sharifi <massoud@truetale.io>
2023-11-07 10:22:08 +0430

    update material of imported models

--- src/App.js
+++ src/App.js
@@ -7,7 +7,9 @@ import AppLoading from "./projects/3d-app-loading";
 import CameraMovementGSAP from "./projects/camera-movement-gsap";
 import Office from "./projects/office";
 import Booth from "./projects/booth";
+import Responsive from "./projects/responsive";
+import Audio from "./projects/audio";
 
 export default function App() {
-  return <Booth />;
+  return <Audio />;
 }
